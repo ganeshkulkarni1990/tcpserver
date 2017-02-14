84c73ff5cacfeb83546131b53ecda851c2383fa9
ganeshkulkarni1990 <ganeshkulkarni1990@gmail.com>
2017-02-15 03:13:56 +0530

    math all

--- math.c
+++ math.c
@@ -5,9 +5,15 @@ int add(int a,int b)
 {
 	return a+b;
 }
+
+int sub(int a,int b)
+{
+	return a-b;
+}
 int main()
 {
 	int a=10,b=20;
 	printf("%d\n",add(a,b));
+	printf("%d\n",sub(a,b));
 	return 1;
 }
